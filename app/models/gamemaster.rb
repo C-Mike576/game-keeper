741f660f62160acfede5ebc8a91e74b7c0b7dd99
C-Mike576 <m.calkins576@gmail.com>
2020-02-11 14:37:01 -0500

    Add table colomums

--- app/models/gamemaster.rb
+++ app/models/gamemaster.rb
@@ -1,4 +1,5 @@
 class Gamemaster < ApplicationRecord
+    belongs_to :user
     has many :notes
     has_many :players, through: :notes
 end
--- db/migrate/20200211185657_create_gamemasters.rb
+++ db/migrate/20200211185657_create_gamemasters.rb
@@ -2,6 +2,7 @@ class CreateGamemasters < ActiveRecord::Migration[6.0]
   def change
     create_table :gamemasters do |t|
       t.string :name
+      t.integer :user_id
 
       t.timestamps
     end
