33cdb24d33fb701e5aa17e94d03b760679ee6176
C-Mike576 <m.calkins576@gmail.com>
2020-02-12 14:10:03 -0500

    adding controller function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,32 @@
 class UsersController < ApplicationController
 
     def show
-    
+        @user = User.find_by_id(paramas[:id])
     end
 
     def new
-        @user = User.new
+        if logged_in?
+            redirect_to user_path(current_user)
+        else
+            @user = User.new
+        end
     end
 
     def create 
-    
+        binding.pry
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
     end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :role, :password, :email)
+  end
 
 end
