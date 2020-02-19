66998176fbc9870d15893122cfbf64c64b5502fa
C-Mike576 <m.calkins576@gmail.com>
2020-02-19 14:31:45 -0500

    added google signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,27 @@ class SessionsController < ApplicationController
     end
   end
 
+  def omniauth
+    @user = User.find_or_create_by(uid: auth[:uid]) do |u|
+      u.username = auth[:info][:email]
+      u.email = auth[:info][:email]
+      u.password = SecureRandom.hex
+    end
+
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] = @user.errors.full_messages
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get '/login', to: 'sessions#new', as: '/login'
   post '/login', to: 'sessions#create'
-  
+
+  get '/auth/:provider/callback' => 'sessions#omniauth'
+
   resources :gamemasters, only: [:new, :create, :show, :edit, :update] do
     resources :notes
   end
